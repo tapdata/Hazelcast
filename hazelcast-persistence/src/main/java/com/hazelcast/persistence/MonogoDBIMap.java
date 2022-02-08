7847f983ce114e799527749edb9e993e3a46f004
samuel <sam@tapdata.io>
2022-02-08 15:36:39 +0800

    persistence的imap支持其他格式的value

--- hazelcast-persistence/src/main/java/com/hazelcast/persistence/MonogoDBIMap.java
+++ hazelcast-persistence/src/main/java/com/hazelcast/persistence/MonogoDBIMap.java
@@ -16,97 +16,100 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Properties;
 
-public class MonogoDBIMap implements MapStore<String, Document>, MapLoaderLifecycleSupport {
-    private MongoClient mongoClient;
-    private MongoCollection<Document> cacheCollection;
-    private final Long autoCreateIndexDocumentLimit = 5000000L;
-    private final String defaultMongoUri = "mongodb://127.0.0.1";
-    private final String defaultMongoDB = "cache";
-    private final String defaultMongoCollection = "imap";
-    private String imapName;
-    private Document sign;
+public class MonogoDBIMap implements MapStore<String, Object>, MapLoaderLifecycleSupport {
+	private MongoClient mongoClient;
+	private MongoCollection<Document> cacheCollection;
+	private final Long autoCreateIndexDocumentLimit = 5000000L;
+	private final String defaultMongoUri = "mongodb://127.0.0.1";
+	private final String defaultMongoDB = "cache";
+	private final String defaultMongoCollection = "imap";
+	private String imapName;
+	private Document sign;
 
-    private Document sign() {
-        return new Document(sign);
-    }
+	private Document sign() {
+		return new Document(sign);
+	}
 
-    public MonogoDBIMap() {
-    }
+	public MonogoDBIMap() {
+	}
 
-    @Override
-    public void init(HazelcastInstance hazelcastInstance, Properties properties, String s) {
-        String mongoUri = properties.getProperty("mongo.uri");
-        if (mongoUri == null) {
-            mongoUri = defaultMongoUri;
-        }
-        String db = properties.getProperty("mongo.db");
-        if (db == null) {
-            db = defaultMongoDB;
-        }
-        String collection = properties.getProperty("mongo.collection");
-        if (collection == null) {
-            collection = defaultMongoCollection;
-        }
+	@Override
+	public void init(HazelcastInstance hazelcastInstance, Properties properties, String s) {
+		String mongoUri = properties.getProperty("mongo.uri");
+		if (mongoUri == null) {
+			mongoUri = defaultMongoUri;
+		}
+		String db = properties.getProperty("mongo.db");
+		if (db == null) {
+			db = defaultMongoDB;
+		}
+		String collection = properties.getProperty("mongo.collection");
+		if (collection == null) {
+			collection = defaultMongoCollection;
+		}
 
-        mongoClient = new MongoClient(new MongoClientURI(mongoUri));
-        cacheCollection = mongoClient.getDatabase(db).getCollection(collection);
+		mongoClient = new MongoClient(new MongoClientURI(mongoUri));
+		cacheCollection = mongoClient.getDatabase(db).getCollection(collection);
 
-        Long cacheCollectionCount = cacheCollection.countDocuments();
-        if (cacheCollectionCount > autoCreateIndexDocumentLimit) {
-            throw new RuntimeException(String.format("mongo uri: %s, db: %s, collection: %s config as cache collection, but no index on key field, and because its document count is too many, %d: more than: %d, we stop auto create it, please manual create index with {\"key\":1}", mongoUri, db, collection, cacheCollectionCount, autoCreateIndexDocumentLimit));
-        }
-        Bson keyIndex = Indexes.ascending("key", "imap");
-        cacheCollection.createIndex(keyIndex);
-        this.imapName = s;
-        sign = new Document("imap", this.imapName);
-    }
+		Long cacheCollectionCount = cacheCollection.countDocuments();
+		if (cacheCollectionCount > autoCreateIndexDocumentLimit) {
+			throw new RuntimeException(String.format("mongo uri: %s, db: %s, collection: %s config as cache collection, but no index on key field, and because its document count is too many, %d: more than: %d, we stop auto create it, please manual create index with {\"key\":1}", mongoUri, db, collection, cacheCollectionCount, autoCreateIndexDocumentLimit));
+		}
+		Bson keyIndex = Indexes.ascending("key", "imap");
+		cacheCollection.createIndex(keyIndex);
+		this.imapName = s;
+		sign = new Document("imap", this.imapName);
+	}
 
-    @Override
-    public void destroy() {
-        this.mongoClient.close();
-    }
+	@Override
+	public void destroy() {
+		this.mongoClient.close();
+	}
 
-    public synchronized void store(String key, Document value) {
-        Document query = sign().append("key", key);
-        Document doc = new Document(query).append("value", value);
-        ReplaceOptions options = new ReplaceOptions().upsert(true);
-        cacheCollection.replaceOne(query, doc, options);
-    }
+	public synchronized void store(String key, Object value) {
+		if (value instanceof Document) {
+			Document query = sign().append("key", key);
+			Document doc = new Document(query).append("value", value);
+			ReplaceOptions options = new ReplaceOptions().upsert(true);
+			cacheCollection.replaceOne(query, doc, options);
+		}
+		// TODO 其余类型暂时不持久化
+	}
 
-    public synchronized void storeAll(Map<String, Document> map) {
-        for (Map.Entry<String, Document> entry : map.entrySet()) {
-            store(entry.getKey(), entry.getValue());
-        }
-    }
+	public synchronized void storeAll(Map<String, Object> map) {
+		for (Map.Entry<String, Object> entry : map.entrySet()) {
+			store(entry.getKey(), entry.getValue());
+		}
+	}
 
-    public synchronized void delete(String key) {
-        cacheCollection.deleteOne(sign().append("key", key));
-    }
+	public synchronized void delete(String key) {
+		cacheCollection.deleteOne(sign().append("key", key));
+	}
 
-    public synchronized void deleteAll(Collection<String> keys) {
-        for (String key : keys) {
-            delete(key);
-        }
-    }
+	public synchronized void deleteAll(Collection<String> keys) {
+		for (String key : keys) {
+			delete(key);
+		}
+	}
 
-    public synchronized Document load(String key) {
-        Document query = sign().append("key", key);
-        Document doc = cacheCollection.find(query).first();
-        if (doc == null) {
-            return null;
-        }
-        return (Document) doc.get("value");
-    }
+	public synchronized Document load(String key) {
+		Document query = sign().append("key", key);
+		Document doc = cacheCollection.find(query).first();
+		if (doc == null) {
+			return null;
+		}
+		return (Document) doc.get("value");
+	}
 
-    public synchronized Map<String, Document> loadAll(Collection<String> keys) {
-        Map<String, Document> result = new HashMap<>();
-        for (String key : keys) {
-            result.put(key, load(key));
-        }
-        return result;
-    }
+	public synchronized Map<String, Object> loadAll(Collection<String> keys) {
+		Map<String, Object> result = new HashMap<>();
+		for (String key : keys) {
+			result.put(key, load(key));
+		}
+		return result;
+	}
 
-    public Iterable<String> loadAllKeys() {
-        return null;
-    }
+	public Iterable<String> loadAllKeys() {
+		return null;
+	}
 }
